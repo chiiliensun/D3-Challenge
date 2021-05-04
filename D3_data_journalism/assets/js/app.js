731f9d46ed2bd9414d253fefc7402411a78deb6c
Chii-Lien Sun <chiilien.sun@gmail.com>
2021-05-04 12:17:01 -0700

    added in activity code from 03-Activites-09-Stu_Hair_Metal to start the bubbles

--- D3_data_journalism/assets/js/app.js
+++ D3_data_journalism/assets/js/app.js
@@ -68,4 +68,53 @@ d3.csv("D3_data_journalism/assets/data/data.csv").then(function(censusData) {
     .classed("axis", true)
     .call(leftAxis);
 
+    // Step 5: Create Circles
+    // ==============================
+    const circlesGroup = chartGroup.selectAll("circle")
+    .data(hairData)
+    .join("circle")
+    .attr("cx", d => xLinearScale(d.hair_length))
+    .attr("cy", d => yLinearScale(d.num_hits))
+    .attr("r", "15")
+    .attr("fill", "pink")
+    .attr("opacity", 0.5)
+    .attr("stroke", "black")
+    .attr("stroke-width", 1);
+
+    // Step 6: Initialize tool tip
+    // ==============================
+    const toolTip = d3.tip()
+      .attr("class", "tooltip")
+      .offset([80, -60])
+      .html(d => `${d.rockband}<br>Hair length: ${d.hair_length}<br>Hits: ${d.num_hits}`);
+
+    // Step 7: Create tooltip in the chart
+    // ==============================
+    chartGroup.call(toolTip);
+
+    // Step 8: Create event listeners to display and hide the tooltip
+    // ==============================
+    circlesGroup.on("click", function(data) {
+      toolTip.show(data, this);
+    })
+      // onmouseout event
+      .on("mouseout", function(data) {
+        toolTip.hide(data);
+      });
+
+    // Create axes labels
+    chartGroup.append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 0 - margin.left + 40)
+      .attr("x", 0 - (height / 2))
+      .attr("dy", "1em")
+      .attr("class", "axisText")
+      .text("Number of Billboard 100 Hits");
+
+    chartGroup.append("text")
+      .attr("transform", `translate(${width / 2}, ${height + margin.top + 30})`)
+      .attr("class", "axisText")
+      .text("Hair Metal Band Hair Length (inches)");
+  }).catch(error => console.log(error));
+
 }).catch(error => console.log(error));
